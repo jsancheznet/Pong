522b01ae8c17553ad756e396761aed170aa177d8
Jorge Sanchez <jsanchezsilvera@gmail.com>
2017-07-13 18:52:55 -0300

    Added SDL Image, Succesfully loaded an image file from an XPM file, embedding the image in the executable

--- build.bat
+++ build.bat
@@ -3,14 +3,16 @@
 pushd build
 
 set SDLINCLUDE="../includes/SDL2-2.0.5/include"
+set SDLIMAGE="../includes/SDL2_image-2.0.1/include"
 set LIB64="../lib/x64/Debug/"
+set SDLIMAGELIB="../includes/SDL2_image-2.0.1/lib/x64"
 set INCLUDES="../includes/"
 
-set IncludeDirectories=-I%SDLINCLUDE% -I%INCLUDES%
-set LibDirectories=-LIBPATH:%LIB64%
+set IncludeDirectories=-I%SDLINCLUDE% -I%SDLIMAGE% -I%INCLUDES%
+set LibDirectories=-LIBPATH:%LIB64% -LIBPATH:%SDLIMAGELIB%
 
 set CompilerFlags= -DDEBUG -nologo -W4 -WX -Od -Ob0 %IncludeDirectories% -Zi -MTd /D "_WINDOWS" /D "_DEBUG"
-set LinkerFlags=-nologo -DEBUG %LibDirectories% SDL2.lib SDL2main.lib
+set LinkerFlags=-nologo -DEBUG %LibDirectories% SDL2.lib SDL2main.lib SDL2_image.lib
 
 cl ..\main.cpp %CompilerFlags% /link %LinkerFlags% -SUBSYSTEM:CONSOLE
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
+// TODO: Statically Link Executable
 #pragma warning(disable: 4189)
 #pragma warning(disable: 4201)
 #pragma warning(disable: 4100)
 
 #pragma comment(lib, "Shcore.lib")
+
 #include <windows.h>
 #include <ShellScalingAPI.h>
-#include <comdef.h>
 #include <SDL.h>
+#include "../assets/background.xpm"
+#include <SDL_image.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <assert.h>
@@ -23,99 +26,44 @@ typedef float    r32;
 typedef double   r64;
 typedef i32      b32;
 
-// TODO: Statically Link Executable
+enum state
+{
+    State_Game,
+    State_Menu,
+    State_Pause,
+    State_End
+};
+
+// struct ball
+// {
+//     v2 Position;
+//     r32 Radius;
+//     r32 r, g, b, a;
+// };
+
+struct gamestate
+{
+    state CurrentState;
+};
+
 
 static i32 IsRunning = true;
-
-union color
-{
-    struct
-    {
-        r32 r, g, b, a;
-    };
-};
-
-struct paddle
-{
-    i32 Side; // 0 == Left, 1 == Right
-    color Color;
-    SDL_Rect Rect;
-    const int Width = 100;
-    const int Height = 100;
-    const int HalfWidth = Width / 2;
-    const int HalfHeight = Height / 2;
-};
-
-void MySDL_GetDisplayDPI(i32 DisplayIndex, r32 *DPI, r32 *DefaultDPI)
-{
-    // High DPI Resources
-    // https://seabird.handmade.network/blogs/p/2460-be_aware_of_high_dpi
-    // https://nlguillemot.wordpress.com/2016/12/11/high-dpi-rendering/
-
-#ifdef __APPLE__
-    const float SystemDefaultDPI = 72.0f;
-#elif defined(_WIN32)
-    const float SystemDefaultDPI = 96.0f;
-#else
-    static_assert(false, "No system default DPI set for this platform.");
-#endif
-
-    if(SDL_GetDisplayDPI(DisplayIndex, NULL, DPI, NULL) != 0)
-    {
-        // Failed to get DPI, just return the deault value
-        printf("SDL_GetDisplayDPI Failed: %s\n", SDL_GetError());
-
-        if(DPI)
-        {
-            *DPI = SystemDefaultDPI;
-        }
-    }
-
-    if(DefaultDPI)
-    {
-        *DefaultDPI = SystemDefaultDPI;
-    }
-}
-
-SDL_Window *MySDL_CreateDpiAwareWindow(char *Title, i32 x, i32 y, i32 w, i32 h, u32 Flags)
-{
-    r32 DPI, DefaultDpi;
-    MySDL_GetDisplayDPI(0, &DPI, &DefaultDpi);
-
-    int WindowScaledWidth = (int)(w * DPI / DefaultDpi);
-    int WindowScaledHeight = (int)(h * DPI / DefaultDpi);
-
-    SDL_Window *Window = SDL_CreateWindow("Pong",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          WindowScaledWidth, WindowScaledHeight,
-                                          SDL_WINDOW_ALLOW_HIGHDPI);
-    return Window;
-}
+static i32 WindowWidth = 640;
+static i32 WindowHeight = 480;
+static gamestate Gamestate = {};
 
 i32 main(i32 argc, char **argv)
 {
-
-    // Enable DPI Awareness
-    SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
-
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *Window = MySDL_CreateDpiAwareWindow("Pong",
-                                                    SDL_WINDOWPOS_CENTERED,
-                                                    SDL_WINDOWPOS_CENTERED,
-                                                    640, 480,
-                                                    SDL_WINDOW_ALLOW_HIGHDPI);
-
+    SDL_Window *Window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          WindowWidth, WindowHeight, NULL);
     SDL_Renderer *Renderer = SDL_CreateRenderer(Window, -1,
                                                 SDL_RENDERER_ACCELERATED |
                                                 SDL_RENDERER_PRESENTVSYNC);
 
-    if(SDL_SetRenderDrawBlendMode(Renderer, SDL_BLENDMODE_BLEND))
-    {
-        // Error!
-        return 0;
-    }
+    SDL_Surface *Image = IMG_ReadXPMFromArray(background_xpm);
+    SDL_Texture *Texture = SDL_CreateTextureFromSurface(Renderer, Image);
 
     while(IsRunning)
     {
@@ -144,12 +92,17 @@ i32 main(i32 argc, char **argv)
             }
         }
 
+        SDL_Rect Rect = {0, 0, 100, 100};
+
         // Render
         SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 255);
         SDL_RenderClear(Renderer);
 
-        SDL_SetRenderDrawColor(Renderer, 255, 0, 255, 255);
+        SDL_RenderCopy(Renderer,
+                       Texture, 0, 0);
 
+        SDL_SetRenderDrawColor(Renderer, 255, 0, 255, 255);
+        SDL_RenderDrawRect(Renderer, &Rect);
         SDL_RenderPresent(Renderer);
     }
 
