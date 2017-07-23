289a508a19d9fe7930895f7c5588909a703d1811
Jorge Sanchez <jsanchezsilvera@gmail.com>
2017-07-22 23:07:00 -0300

    Removed SDL libraries from project, they exist outside and must be provided in compile time

--- build.bat
+++ build.bat
@@ -2,20 +2,22 @@
 
 pushd build
 
-set SDLINCLUDE="../includes/SDL2-2.0.5/include"
-set SDLIMAGE="../includes/SDL2_image-2.0.1/include"
-set SDLTTF="../includes/SDL2_ttf-2.0.14/include"
-set LIB64="../lib/x64/Debug/"
-set SDLIMAGELIB="../includes/SDL2_image-2.0.1/lib/x64"
-set SDLTTFLIB="../includes/SDL2_ttf-2.0.14/lib/x64"
-set SDLMIXERINCLUDE="../includes/SDL2_mixer-2.0.1/include"
-set SDLMIXERLIB="../includes/SDL2_mixer-2.0.1/lib/x64"
-set INCLUDES="../includes/"
+set SDLINCLUDE="C:\Users\Jsanchez\Dropbox\Projects\Libraries\SDL2-devel-2.0.5-VC\SDL2-2.0.5\include"
+set SDLLIB64="C:\Users\Jsanchez\Dropbox\Projects\Libraries\SDL2-devel-2.0.5-VC\SDL2-2.0.5\lib\x64"
 
-set IncludeDirectories=-I%SDLINCLUDE% -I%SDLIMAGE% -I%SDLTTF% -I%SDLMIXERINCLUDE% -I%INCLUDES%
-set LibDirectories=-LIBPATH:%LIB64% -LIBPATH:%SDLIMAGELIB% -LIBPATH:%SDLTTFLIB% -LIBPATH:%SDLMIXERLIB%
+set SDLIMAGEINCLUDE="C:\Users\Jsanchez\Dropbox\Projects\Libraries\SDL2_image-devel-2.0.1-VC\SDL2_image-2.0.1\include"
+set SDLIMAGELIB64="C:\Users\Jsanchez\Dropbox\Projects\Libraries\SDL2_image-devel-2.0.1-VC\SDL2_image-2.0.1\lib\x64"
 
-set CompilerFlags= -DDEBUG -nologo -W4 -WX -Od -Ob0 %IncludeDirectories% -Zi -MTd /D "_WINDOWS" /D "_DEBUG"
+set SDLTTFINCLUDE="C:\Users\Jsanchez\Dropbox\Projects\Libraries\SDL2_ttf-devel-2.0.14-VC\SDL2_ttf-2.0.14\include"
+set SDLTTFLIB64="C:\Users\Jsanchez\Dropbox\Projects\Libraries\SDL2_ttf-devel-2.0.14-VC\SDL2_ttf-2.0.14\lib\x64"
+
+set SDLMIXERINCLUDE="C:\Users\Jsanchez\Dropbox\Projects\Libraries\SDL2_mixer-devel-2.0.1-VC\SDL2_mixer-2.0.1\include"
+set SDLMIXERLIB64="C:\Users\Jsanchez\Dropbox\Projects\Libraries\SDL2_mixer-devel-2.0.1-VC\SDL2_mixer-2.0.1\lib\x64"
+
+set IncludeDirectories=-I%SDLINCLUDE% -I%SDLIMAGEINCLUDE% -I%SDLTTFINCLUDE% -I%SDLMIXERINCLUDE%
+set LibDirectories=-LIBPATH:%SDLLIB64% -LIBPATH:%SDLIMAGELIB64% -LIBPATH:%SDLTTFLIB64% -LIBPATH:%SDLMIXERLIB64%
+
+set CompilerFlags= -DDEBUG -nologo -W4 -WX -Ox -Ob0 %IncludeDirectories% -Zi -MTd /D "_WINDOWS" /D "_DEBUG"
 set LinkerFlags=-nologo -DEBUG %LibDirectories% SDL2.lib SDL2main.lib SDL2_image.lib SDL2_ttf.lib SDL2_mixer.lib
 
 cl ..\main.cpp %CompilerFlags% /link %LinkerFlags% -SUBSYSTEM:CONSOLE
